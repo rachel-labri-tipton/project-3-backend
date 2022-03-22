58645db524364512992f33ae28ef2b13552872e2
Rachel Tipton <RACHELMTIPTON@GMAIL.COM>
2022-03-22 22:24:02 +0100

    pushing updated router

---
+++ controllers/userController.js
@@ -0,0 +1,76 @@
+// import User...
+// import bcrypt from "bcrypt"
+// import jwt from "jsonwebtoken"
+async function index(req, res) {
+    try {
+        const users = await User.find()
+        res.send(users)
+    } catch (e) {
+        next(err)
+    }
+}
+
+async function register(req, res, next) {
+    try {
+        const existingUser = await User.findOne({ eMail: req.body.eMail })
+        console.log(existingUser)
+        if (existingUser) {
+            return res.status(400).json({ message: "This user already exists." })
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        const newUser = await User.create({ ...req.body, password: hashedPassword })
+        res.status(201).json(newUser)
+    } catch (err) {
+        next(err)
+    }
+}
+
+async function remove(req, res, next) {
+    const id = req.params.id
+    try {
+        const deletedUser = await User.findOneAndDelete({ _id: id })
+        if (!deletedUser) return res.json({ message: "deleted user not found" })
+        console.log(deletedUser)
+        if (deletedUser) return res.json({ message: ` ${deletedUser.userName} has been deleted` })
+        res.sendStatus(204)
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+async function login(req, res, next) {
+    try {
+        const user = await User.findOne({ userName: req.body.userName })
+        console.log(user)
+        if (!user) {
+            return res.status(400).json({ message: "Invalid Credentials." })
+        }
+        console.log(user)
+        const passwordsMatch = await bcrypt.compare(req.body.password, user.password)
+        if (!passwordsMatch) {
+            return res.json({ message: "Wrong password" })
+        }
+        const payload = {
+            userName: user.userName,
+            role: user.role,
+        }
+        //don't put too much sensitive information in the payload that comes back
+        //here we sign with a secret the given payload 
+        const token = jwt.sign(payload, process.env.JWT_SECRET)
+        console.log(token)
+        res.status(200).json({ token })
+    } catch (err) {
+        next(err)
+    }
+
+}
+
+export default {
+    index,
+    register, 
+    login, 
+    remove
+}
+
--- views/router.js
+++ views/router.js
@@ -1,20 +1,25 @@
 import express from "express"
 // ? Import all my controllers
-import dataController from "../controllers/dataController.js"
+// import dataController from "../controllers/dataController.js"
+import recipeController from "../controllers/recipeController.js"
 
 // ? This is a nice router object express gives you. Syntactic sugar
 // ? to set up routes.
 const router = express.Router()
 
-// ? Routes/Views: This is like app.get
-router.route("/movies")
-    .get(movieController.index)
-    .post(() => console.log("TODO: post a movie"))
+//Routes for recipeController
+router.route("/recipes")
+    .get(recipeController.index)
+    .post(recipeController.create)
 
-router.route("/movie/:id")
-    .get(() => console.log("TODO: get a single movie"))
-    .delete(() => console.log("TODO: delete a movie"))
-    .put(() => console.log("TODO: update a movie"))
+router.route("/recipes/:id")
+    .get(recipeController.show)
+    .delete(recipeController.remove)
+    .put(recipeController.update)
+
+router.route("/recipes/:id/review")
+
+
 
 // ? Finally export my router.
 // ! Make sure its lower case
