0db5f5339f9666ad7146a0b8f46c25540d59411e
Emilie Allen <emilie.allen@hotmail.co.uk>
2022-03-26 14:48:13 +0000

    adding cors

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ import express from "express"
 import { connectToDb } from "./db/helpers.js"
 import logger from "./middleware/logger.js"
 import errorHandler from "./middleware/errorHandler.js"
+import cors from "cors"
+import dotenv from "dotenv"
+
 // ? Setting up express ready to use
 
 async function startServer() {
 
     const app = express()
+
+    dotenv.config()
+
+    const PORT = process.env.PORT ? process.env.PORT : 4000
+
+    app.use(cors())
 
     // ! Tell express it's a JSON API
     app.use(express.json())
